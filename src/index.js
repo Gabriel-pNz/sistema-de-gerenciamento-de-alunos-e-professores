baeffb24a1089354fe364a40ca6605b2333610c1
Gabriel-pNz <gabriell.passos7@gmail.com>
2023-09-15 15:59:02 -0300

    utils adicionado para leitura/escritas de arquivos com lib fs/promises

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 const express = require('express');
+const { readFile, writeFile } = require('./utils/fileUtils');
 const app = express();
 
 app.use(express.json());
 
+app.post('/aluno', async(req, res) => {
+
+    const { nome, email, matricula, data_nascimento} = req.body;
+
+    const alunosLista = await readFile('./src/dataBase/alunosData.json')
+
+    alunosLista.alunos.push({
+        nome,
+        email,
+        matricula,
+        data_nascimento
+    })
+    
+    await writeFile('./src/dataBase/alunosData.json', alunosLista);
+    res.status(201).send()
+    
+})
+
 app.listen(3000);
+
+
+
---
+++ src/utils/fileUtils.js
@@ -0,0 +1,27 @@
+const fs = require('fs/promises');
+
+async function readFile(filePath) {
+    try {
+        const data = await fs.readFile(filePath);
+        return JSON.parse(data);
+    } catch (error) {
+        throw new Error(`Error: ${error.message}`);
+    }
+}
+
+async function writeFile(filePath, data) {
+    try {
+        await fs.writeFile(filePath, JSON.stringify(data), 'utf-8');
+    } catch (error) {
+        throw new Error(`Error: ${error.message}`);
+    }
+}
+
+module.exports = { readFile, writeFile };
+
+
+
+
+
+
+
