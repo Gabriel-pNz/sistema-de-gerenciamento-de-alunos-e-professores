285017a68b2e7c7a40b177d4def984559742ecf2
Gabriel-pNz <gabriell.passos7@gmail.com>
2023-09-16 09:45:17 -0300

    routes para recurso aluno criado e importado para arquivo index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
-
+const route = require('./routes/alunoRoute')
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-app.get('/aluno', )
-
-app.get('/aluno/:matricula', )
-
-app.post('/aluno', )
-
-app.put('/aluno/:matricula', );
-
-app.delete('/aluno/:matricula', )
+app.use('/aluno', route);
 
 app.listen(3000);
 
---
+++ src/routes/alunoRoute.js
@@ -0,0 +1,14 @@
+const route = require('express').Router();
+const alunoController = require('../controllers/alunoController');
+
+route.get('/', alunoController.listaAlunos);
+
+route.get('/:matricula', alunoController.buscarAluno);
+
+route.post('/', alunoController.cadastrarAluno);
+
+route.put('/:matricula', alunoController.atualizarDadosAluno);
+
+route.delete('/:matricula', alunoController.deletarAluno);
+
+module.exports = route;
